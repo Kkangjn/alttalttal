91553cab92fcadbb543acac1cf7e925d18cc07f8
Kkangjn <jjun1401@naver.com>
2023-07-15 17:04:21 +0900

    [REFACTOR] RecipeService recipeRepository.findById(id)를 private 메소드로 만듦

--- src/main/java/com/alttalttal/mini_project/service/RecipeService.java
+++ src/main/java/com/alttalttal/mini_project/service/RecipeService.java
@@ -11,14 +11,18 @@ import org.springframework.stereotype.Service;
 public class RecipeService {
     private final RecipeRepository recipeRepository;
     public RecipeResponseDto getRecipe(Long id, Long userId) {
-        Recipe recipe = recipeRepository.findById(id).orElseThrow(() ->
-                new IllegalArgumentException("잘못된 접근입니다.")
-        );
+        Recipe recipe = findRecipeById(id);
         // 유저가 찜 했는지 확인
         Boolean isUserZzim = recipe.getZzim().stream().anyMatch(zzim -> zzim.getUserId() == userId);
         // 찜한 유저수(인기 레시피를 고려해서 만듦)
         Integer countZzim = recipe.getZzim().size();
 
         return new RecipeResponseDto(recipe, isUserZzim, countZzim);
+    }
+
+    private Recipe findRecipeById(Long id){
+        return recipeRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("잘못된 접근입니다.")
+        );
     }
 }
