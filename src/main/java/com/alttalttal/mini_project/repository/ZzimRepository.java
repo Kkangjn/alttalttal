f014c0cc0ec7f00024ba4d3f5bbbee086ba9550b
Kkangjn <jjun1401@naver.com>
2023-07-15 17:05:40 +0900

    [ADD] 찜하기 기능 관련

---
+++ src/main/java/com/alttalttal/mini_project/repository/ZzimRepository.java
@@ -0,0 +1,7 @@
+package com.alttalttal.mini_project.repository;
+
+import com.alttalttal.mini_project.entity.Zzim;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZzimRepository extends JpaRepository<Zzim, Long> {
+}
