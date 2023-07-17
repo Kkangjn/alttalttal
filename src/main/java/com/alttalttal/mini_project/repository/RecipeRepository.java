2f3fc7024de644b779a4840cde586388aa8fcee2
Kkangjn <jjun1401@naver.com>
2023-07-17 17:04:52 +0900

    [REMOVE] mongoDB 조회 테스트 관련, 기존 mysql recipe관련

--- src/main/java/com/alttalttal/mini_project/repository/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alttalttal.mini_project.repository;
-
-import com.alttalttal.mini_project.entity.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
