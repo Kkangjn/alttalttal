d932f91185a9edaf06a592fa9b9d8cb04d0ed67b
Mi <mi_aa_@naver.com>
2023-09-25 21:35:57 +0900

    [REFACTOR] 필요없는 부분 삭제 및 필요한 부분 추가

--- src/main/java/com/alttalttal/mini_project/service/RedisService.java
+++ src/main/java/com/alttalttal/mini_project/service/RedisService.java
@@ -1,6 +1,5 @@
 package com.alttalttal.mini_project.service;
 
-import com.alttalttal.mini_project.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
